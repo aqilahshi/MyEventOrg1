faaf2bc27b5d89ecf6adae4e0c918b8ec25c396a
zakirahsharifuddin <zakirahsharifuddin@student.usm.my>
2023-03-31 18:08:56 +0800

    update box event

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BiDotsHorizontal } from "react-icons/bi";
 
 function Box() {
    
     return (
         <div className="small_box">
-            
+            <div className="box_content">
+                <a class="dot_menu" href="#"><BiDotsHorizontal/></a> 
+                <img class="image" src="logo192.png"></img>
+            </div>
         </div>
     );
 }
