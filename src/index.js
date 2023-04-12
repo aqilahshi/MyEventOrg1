1b992f533f8189ac39c9560dc9126628e48ccccf
zakirahsharifuddin <zakirahsharifuddin@student.usm.my>
2023-04-12 22:10:52 +0800

    update chat

--- src/index.js
+++ src/index.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
+
+
+
+
 import reportWebVitals from './reportWebVitals';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthContextProvider>
+     <ChatContextProvider>
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+     </ChatContextProvider>
+   </AuthContextProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <AuthContextProvider>
+//     <ChatContextProvider>
+//       {/* <React.StrictMode>
+//         <App />
+//       </React.StrictMode> */}
+//     </ChatContextProvider>
+//   </AuthContextProvider>
+// );
