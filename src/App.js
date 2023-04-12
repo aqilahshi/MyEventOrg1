2fa92d44950d6fa2a8197f39d7d7941cfa76a390
zakirahsharifuddin <zakirahsharifuddin@student.usm.my>
2023-04-12 22:11:45 +0800

    update chat

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Calls from './pages/Todo.jsx';
-import Chat from './pages/Chat.jsx';
+import Comm from './pages/Comm.jsx';
 import Calendars from './pages/Calendars.jsx';
 import Files from './pages/Files.jsx';
 import Applications from './pages/Shop.jsx';
@@ -45,13 +45,110 @@ import ApproveVendor from "./pages/ApproveVendor";
 import CreatePost from "./pages/CreatePost";
 
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Sidebar>
+//         <Routes>
+//           <Route path="/" element={<MyEvent />} />
+//           <Route path="/comm" element={<Comm />} />
+//           <Route path="/calls" element={<Calls />} />
+//           <Route path="/calendars" element={<Calendars />} />
+//           <Route path="/files" element={<Files />} />
+//           <Route path="/applications" element={<Applications />} />
+//           <Route path="/myevent" element={<MyEvent />} />
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/myevent/addevent" element={<AddEvent />} />
+//           <Route path="/attendance" element={<Attendance />} />
+//           <Route path="/award" element={<Award />} /> 
+//           <Route path="/winnerdetails" element={<Winner />} /> 
+//           <Route path="/LivePoll" element={<Lvquest />} />
+//           <Route path="/awardpage" element={<AwardPart />} />
+//           <Route path="/activity" element={<Activity />} />
+//           <Route path="/createlivepoll" element={<CreateLv />} />
+//           <Route path="/createquiz" element={<CreateQuiz />} />
+//           <Route path="/createquizdetails" element={<DetailsQuizFake />} />
+//           <Route path="/createquiz/createquizdetails" element={<DetailsQuizFake />} />
+//           <Route path="/createluckydraw" element={<CreateLD />} />
+//           <Route path="/resultlivepoll" element={<ResultLv />} />
+//           <Route path="/playlivepoll" element={<PlayLv />} />
+//           <Route path="/luckydraw" element={<LuckyDrawAlgo />} />          
+
+
+//           {/* start kirah part */}
+//           <Route path="/myevent/dashboard" element={<Dashboard />} />
+//           <Route path="/myevent/dashboard/eventsummary" element={<EventSummary />} />
+//           <Route path="/myevent/dashboard/organizationstructure" element={<OrganizationStructure />} />
+//           <Route path="/myevent/dashboard/eventagenda" element={<EventAgenda />} />
+//           <Route path="/myevent/dashboard/budgetplan" element={<BudgetPlan />} />
+//           <Route path="/myevent/dashboard/attendance" element={<Attendance />} />
+//           <Route path="/myevent/dashboard/activity" element={<Activity />} />
+//           <Route path="/myevent/dashboard/award" element={<Award />} />
+//           <Route path="/myevent/dashboard/generatecertificate" element={<GenerateCertificate />} />
+//           <Route path="/myevent/dashboard/generatefinalreport" element={<FinalReport />} />
+//           <Route path="/shop" element={<Shop />} />
+//           <Route path="/cart" element={<Cart />} />
+//           <Route path="/vendorregister" element={<VendorRegister />} />
+//           <Route path="/vendorlogin" element={<VendorLogin />} />
+//           <Route path='/product' element={<Product />} />
+//           <Route path='/productlist' element={<ProductList />} />
+//           <Route path='/createpost' element={<CreatePost />} />
+//           {/* delete */}
+//           {/* edit */}
+//           <Route path='/validatevendor' element={<ValidateVendor />} />
+//           <Route path='/validatevendor/approvevendor' element={<ApproveVendor />} />
+//           {/* categories?? */}
+//           {/* admin dashboard */}
+//           {/* end kirah part */}
+//         </Routes>
+//       </Sidebar>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+import Home from "./pages/Comm";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import "./style.scss";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
     <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<MyEvent />} />
-          <Route path="/chat" element={<Chat />} />
+    <Sidebar>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/" element={<MyEvent />} />
+          <Route path="/comm" element={<Comm />} />
           <Route path="/calls" element={<Calls />} />
           <Route path="/calendars" element={<Calendars />} />
           <Route path="/files" element={<Files />} />
@@ -72,7 +169,10 @@ const App = () => {
           <Route path="/createluckydraw" element={<CreateLD />} />
           <Route path="/resultlivepoll" element={<ResultLv />} />
           <Route path="/playlivepoll" element={<PlayLv />} />
+          <Route path="/luckydraw" element={<LuckyDrawAlgo />} />          
 
+
+          {/* start kirah part */}
           <Route path="/myevent/dashboard" element={<Dashboard />} />
           <Route path="/myevent/dashboard/eventsummary" element={<EventSummary />} />
           <Route path="/myevent/dashboard/organizationstructure" element={<OrganizationStructure />} />
@@ -96,11 +196,11 @@ const App = () => {
           <Route path='/validatevendor/approvevendor' element={<ApproveVendor />} />
           {/* categories?? */}
           {/* admin dashboard */}
-          <Route path="/luckydraw" element={<LuckyDrawAlgo />} />          
-        </Routes>
+          {/* end kirah part */}
+      </Routes>
       </Sidebar>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
